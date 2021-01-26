3fd6b6d9441b904eb36a89f25c8b0a60ec0cc144
crivalgon <crivalgon@gmail.com>
2021-01-26 22:52:05 +0100

    Correccion

--- extra/submit_ochoa_test.sh
+++ extra/submit_ochoa_test.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 #SBATCH -p hpc-bio-ochoa
-#SBATCH --chdir=/home/alumno19/lab3-container/singularity
+#SBATCH --chdir=/home/alumno19/lab3-container/extra
 #SBATCH -J lab3_cvg
 #SBATCH --cpus-per-task=1
 #SBATCH --mail-type=END   # END/START/NONE
@@ -9,7 +9,7 @@
 
 module load singularity/3.7.0
 
-python3.6 ../extra/vecinas_test.py
-singularity exec python_latest.sif python ../extra/vecinas_test.py
+python3.6 vecinas_test.py
+singularity exec ../singularity/python_latest.sif python vecinas_test.py
 
 
